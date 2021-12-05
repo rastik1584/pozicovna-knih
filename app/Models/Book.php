8e1bcaa2e0e1a3132a6b05c15500f88151448191
Rastislav Vozár <rastik1585@gmaik.com>
2021-12-05 15:39:50 +0100

    update book filter query

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -22,10 +22,11 @@ class Book extends Model
         $table = $this->table;
         $query->when($filters['filters'] ?? null, function ($query, $search) use($table) {
             $query->leftJoin((new Author())->getTable().' as author', "$table.author_id", '=', 'author.id')
-                ->where("$table.title", 'like', '%'.$search.'%')
-                ->orWhere("author.name", 'like', '%'.$search.'%')
-                ->orWhere('author.surname', 'like', '%'.$search.'%')
-            ->select('author.name', 'author.surname', "$table.*");
+                ->where(function($query) use ($table, $search) {
+                    $query->where("$table.title", 'like', '%'.$search.'%')
+                        ->orWhere("author.name", 'like', '%'.$search.'%')
+                        ->orWhere('author.surname', 'like', '%'.$search.'%');
+                })->select('author.name', 'author.surname', "$table.*");
         })->when($filters['borrowed_filter'] ?? null, function ($query, $borrowed) {
             $is_borrowed = $borrowed ? 1 : 0;
             $query->where('is_borrowed', $is_borrowed);
